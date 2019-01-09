d28d6328597c076b86bf7c925d5628c6fe8a5105
burlingk <burlingk@cagox.net>
2019-01-09 01:40:44 -0800

    Added a few empty packages. * Created the stub for package game. (i.e. doc.go) * Created the stub for the package thread.

---
+++ models/game/doc.go
@@ -0,0 +1,6 @@
+/*
+Package game will hold the object and associated logic for the Game model.
+
+game.Game will be what everything else game related hangs off of.
+*/
+package game
---
+++ models/thread/doc.go
@@ -0,0 +1,4 @@
+/*
+Package thread will hold the basic models and logic for threads and posts.
+*/
+package thread
